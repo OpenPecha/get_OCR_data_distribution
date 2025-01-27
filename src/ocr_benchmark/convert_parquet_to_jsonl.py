52782fea98340deaa43daf4fb9cb0b4259e718f0
tenkalden <tenkalden007@gmail.com>
2025-01-27 11:51:25 +0530

    updated parquet to jsonl conversion module

--- src/ocr_benchmark/convert_parquet_to_jsonl.py
+++ src/ocr_benchmark/convert_parquet_to_jsonl.py
@@ -4,15 +4,36 @@ import os
 
 
 def convert_to_jsonl(df, output_path):
-    with open(output_path, 'w', encoding='utf-8') as jsonl_file:
+    with open(output_path, "w", encoding="utf-8") as jsonl_file:
         for record in df.to_dict(orient="records"):
             json.dump(record, jsonl_file, ensure_ascii=False)
-            jsonl_file.write('\n')
+            jsonl_file.write("\n")
 
 
-input_file = 'data/OCR-Derge_Tenjur.parquet'
-output_file = 'data/test_data_json/OCR-Dergetenjur.jsonl'
+def convert_parquet_to_jsonl(input_dir, output_dir):
+    for root, _, files in os.walk(input_dir):
+        for file in files:
+            if file.endswith(".parquet"):
+                input_file_path = os.path.join(root, file)
 
-os.makedirs(os.path.dirname(output_file), exist_ok=True)
-df = pd.read_parquet(input_file)
-convert_to_jsonl(df, output_file)
+                relative_path = os.path.relpath(root, input_dir)
+                output_folder = os.path.join(output_dir, relative_path)
+                os.makedirs(output_folder, exist_ok=True)
+
+                output_file_path = os.path.join(
+                    output_folder, os.path.splitext(file)[0] + ".jsonl"
+                )
+
+                df = pd.read_parquet(input_file_path)
+                convert_to_jsonl(df, output_file_path)
+                print(f"Converted: {input_file_path} -> {output_file_path}")
+
+
+def main():
+    input_directory = "data/add_metadata/source_repo"
+    output_directory = "data/add_metadata/jsonl_source_repo"
+    convert_parquet_to_jsonl(input_directory, output_directory)
+
+
+if __name__ == "__main__":
+    main()
